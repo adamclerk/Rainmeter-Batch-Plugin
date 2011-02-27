bb0ce38b2196045882cbf2a6debbf778931a995f
unknown <adamc@.unitymg.com>
2011-02-26 21:52:08 -0700

    adding batch plugin source

---
+++ Shared Source/Main.cs
@@ -0,0 +1,152 @@
+﻿using System;
+
+namespace CSPluginTemplate
+{
+    public class Main
+    {
+        /// <summary>
+        /// Your name (author) and the version of the plugin go here.  The code will
+        /// automatically format the values and send them back when a call to GetAuthor()
+        /// or GetVersion() is made.
+        /// 
+        /// Optionally, you may also provide an e-mail address and additional comments
+        /// </summary>
+        private static Rainmeter.Settings Plugin = new Rainmeter.Settings
+        (
+            // Author name
+            "Adam Clark",
+
+            // Version
+            0.01,
+
+            // E-mail
+            "adamclerk@gmail.com",
+
+            // Comments (try to keep it under 50 characters)
+            "Writes output from batchfile"
+        );
+
+        #region GetAuthor() and GetVersion() exports -- no need to modify
+
+        public static UInt32 GetPluginVersion()
+        {
+            return Rainmeter.Version(Plugin.Version);
+        }
+
+        public unsafe static char* GetPluginAuthor()
+        {
+            if (!string.IsNullOrEmpty(Plugin.Email) && !string.IsNullOrEmpty(Plugin.Comments))
+                return Rainmeter.String(Plugin.Author + " (" + Plugin.Email + "): " + Plugin.Comments);
+            if (!string.IsNullOrEmpty(Plugin.Email))
+                return Rainmeter.String(Plugin.Author + " (" + Plugin.Email + ")");
+            if (!string.IsNullOrEmpty(Plugin.Comments))
+                return Rainmeter.String(Plugin.Author + ": " + Plugin.Comments);
+            return Rainmeter.String(Plugin.Author);
+        }
+
+        #endregion
+
+        #region Initialize() and Finalize() -- you may add to these functions if necessary
+        public unsafe static UInt32 Initialize(IntPtr instance, char* iniFile, char* section, UInt32 id)
+        {
+            Plugin.AddInstance(iniFile, section, id); // required: do not remove
+
+            ////////////////////////////////////////////////////////////////
+            //
+            // You may add code here, if necessary
+            //
+
+            ////////////////////////////////////////////////////////////////
+            return 0;
+        }
+
+        public static void Finalize(IntPtr instance, UInt32 id)
+        {
+            Plugin.RemoveInstance(id); // required: do not remove
+
+            ////////////////////////////////////////////////////////////////
+            //
+            // You may add code here, if necessary
+            //
+
+            ////////////////////////////////////////////////////////////////
+            return;
+        }
+        #endregion
+
+        #region Update(), Update2(), and GetString() exports -- please read notes
+
+        // *** WARNING / NOTES ***
+        //
+        // Do not add to this code:  change your code in PluginCode.cs instead
+        //
+        // However, due to the way Rainmeter works, you will probably want to
+        // comment-out either 'Update' or 'Update2' if your plugin will be returning
+        // numeric values.
+        //
+        // Rainmeter will look for 'Update' for positive integers.  If you want to
+        // allow negative numbers or floating-point numbers, use 'Update2' instead.
+        //
+        // You *MUST* comment-out whichever function you don't want to use for this
+        // to work.
+        //
+        // If you don't care, such as a plugin that either doesn't return data or
+        // only returns string/text data, then you can leave them both here (it won't
+        // hurt anything).
+        //
+        // *** WARNING / NOTES ***
+
+
+        /// <summary>
+        /// Rainmeter's request for numeric data from the plugin.  This version can only
+        /// return positive integers ranging from 0 to 4,294,967,296.  Comment this member
+        /// out and use 'Update2' if you want to return negative or floating-point values.
+        /// </summary>
+        /// <param name="id">The unique instance ID of this request.</param>
+        /// <returns>Current value for this meter.</returns>
+        public static UInt32 Update(UInt32 id)
+        {
+            // Do not modify this member (although you can comment it out).  Instead, update 
+            // your code in 'PluginCode.cs'.
+            return new YourPlugin().Update(Plugin, id);
+        }
+
+        /// <summary>
+        /// Rainmeter's request for numeric data from the plugin.  This version can return
+        /// positive or negative floating-point numbers (32-bit precision).  Comment this
+        /// member out and use 'Update' if you want to only return positive integers.
+        /// </summary>
+        /// <param name="id">The unique instance ID of this request.</param>
+        /// <returns>Current value for this meter.</returns>
+        public static double Update2(UInt32 id)
+        {
+            // Do not modify this member (although you can comment it out).  Instead, update 
+            // your code in 'PluginCode.cs'.
+            return new YourPlugin().Update2(Plugin, id);
+        }
+
+        /// <summary>
+        /// Rainmeter's request for text data from the plugin.
+        /// </summary>
+        /// <param name="id">The unique instance ID of this request.</param>
+        /// <param name="flags">Unused still as of Dec 2nd, 2010.</param>
+        /// <returns></returns>
+        public unsafe static char* GetString(UInt32 id, UInt32 flags)
+        {
+            // Do not modify this member.  Instead, update your code in 'PluginCode.cs'.
+            return Rainmeter.String(new YourPlugin().GetString(Plugin, id));
+        }
+
+        #endregion
+
+        #region ExecuteBang() export -- no need to modify (change code in PluginCode.cs)
+
+        public static unsafe void ExecuteBang(char* args, UInt32 id)
+        {
+            new YourPlugin().ExecuteBang(Plugin, id, new string(args));
+            return;
+        }
+
+        #endregion
+    }
+}
---
+++ Shared Source/PluginCode.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+// The bulk of your plugin's code belongs in this file.
+namespace CSPluginTemplate
+{
+    class PluginCode
+    {
+
+        private string _output = "test";
+        // 'Update', 'Update2', and 'GetString' all return data back to Rainmeter, depending on
+        // if the Rainmeter measure wants a numeric value or a string/text data.
+        //
+        // The 'Instance' member contains all of the data necessary to read the INI file
+        // passed to your plugin when this instance was first initialized.  Remember: your plugin
+        // may be initialized multiple times.  For example, a plugin that reads the free space
+        // of a hard drive may be called multiple times -- once for each installed hard drive.
+
+        public UInt32 Update(Rainmeter.Settings.InstanceSettings Instance)
+        {
+            return 7;
+        }
+
+        public double Update2(Rainmeter.Settings.InstanceSettings Instance)
+        {
+            return 7.0;
+        }
+
+        public string GetString(Rainmeter.Settings.InstanceSettings Instance)
+        {
+            WriteDebug("ConfigName", Instance.ConfigName);
+            WriteDebug("Variable:BatchFile", Instance.INI_Value("BatchFile") );
+
+            var now = DateTime.Now;
+            var lastAccess = Instance.GetVariable("LastAccess");
+            var updateSpan = Convert.ToInt32(Instance.INI_Value("Update"));
+            var wait = Convert.ToInt32(Instance.INI_Value("Update"));
+            if (string.IsNullOrEmpty(lastAccess))
+                wait = 1;
+            else
+            {
+                TimeSpan span = now - Convert.ToDateTime(lastAccess);
+                if (span.TotalMilliseconds > updateSpan)
+                {
+                    wait = 1;
+                }
+            }
+            
+            var file = Instance.INI_Value("BatchFile");
+            System.Threading.Thread.Sleep(wait);
+            Instance.SetVariable("LastAccess", DateTime.Now);
+            return GetBatchString(file);
+
+        }
+
+
+        // 'ExecuteBang' is a way of Rainmeter telling your plugin to do something *right now*.
+        // What it wants to do can be defined by the 'Command' parameter.
+        public void ExecuteBang(Rainmeter.Settings.InstanceSettings Instance, string Command)
+        {
+            return;
+        }
+
+        private string GetBatchString(string path)
+        {
+            var p = new Process
+            {
+                StartInfo =
+                {
+                    CreateNoWindow = true,
+                    WindowStyle = ProcessWindowStyle.Hidden,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    FileName = path
+                }
+            };
+            p.Start();
+            string output = p.StandardOutput.ReadToEnd();
+            p.WaitForExit();
+            return output;
+        }
+
+        private void WriteDebug(string name, string value)
+        {
+            return;
+            var w = new StreamWriter(@"c:\debug.out", true);
+            var n = DateTime.Now;
+            w.WriteLine(n.ToShortDateString() + " " + n.ToShortTimeString() + " Name:" + name +" Value:" + value);
+            w.Close();
+            w.Dispose();
+        }
+    }
+}
